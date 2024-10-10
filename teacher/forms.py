d3ad4cb17928accc3191c14f41ce2d539f4422f1
bhavna <bhavnaparmar920@gmail.com>
2024-10-10 09:53:51 +0530

    bug fix

--- teacher/forms.py
+++ teacher/forms.py
@@ -1,5 +1,5 @@
-from django import forms
-from django.contrib.auth.models import User
+from django import forms # type: ignore
+from django.contrib.auth.models import User # type: ignore
 from . import models
 
 class TeacherUserForm(forms.ModelForm):
